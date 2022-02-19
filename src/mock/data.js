5d3e67d421d2b8ae90a7ce9a7880e2836e928e53
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2022-02-19 12:49:40 -0600

    change title

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,8 +10,8 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Tyler Olson',
+  subtitle: 'FrontEnd Dev',
   cta: '',
 };
 
